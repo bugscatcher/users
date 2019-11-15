dc303cfe3b8a3d6189ad5177f549f26a4832b352
bugscatcher <inurrick@gmail.com>
2019-11-15 20:24:54 +0800

    Add simple test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/Shopify/sarama v1.23.1
+	github.com/bxcodec/faker/v3 v3.2.0
 	github.com/golang/protobuf v1.3.2
 	github.com/google/uuid v1.1.1
 	github.com/jackc/pgx v3.6.0+incompatible
--- server/grpc/users/get_users_test.go
+++ server/grpc/users/get_users_test.go
@@ -2,7 +2,10 @@ package users
 
 import (
 	"context"
+	"github.com/bugscatcher/users/model"
 	"github.com/bugscatcher/users/services"
+	"github.com/bxcodec/faker/v3"
+	"github.com/jackc/pgx"
 	"testing"
 
 	"github.com/bugscatcher/users/application"
@@ -16,13 +19,35 @@ func TestGRPCHandler_GetUsers(t *testing.T) {
 	app, err := application.New(&conf)
 	assert.NoError(t, err)
 	h := New(app)
-
+	user := &model.User{}
+	err = faker.FakeData(&user)
+	assert.NoError(t, err)
+	err = addUser(h.db, user)
+	assert.NoError(t, err)
 	q := &services.QueryUsers{
-		Search: "test",
+		Search: user.FirstName,
 	}
 	result, err := h.GetUsers(context.Background(), q)
 	assert.NoError(t, err)
 	expectedResult := &services.ResultUsers{
-		Users: nil,
+		Users: []*services.User{toGRPCUser(user)},
+	}
+	assert.EqualValues(t, expectedResult, result)
+}
+
+func addUser(pool *pgx.ConnPool, user *model.User) error {
+	_, err := pool.Exec(`
+		INSERT INTO users (id, first_name, last_name, username)
+		VALUES($1, $2, $3, $4)`, user.ID, user.FirstName, user.LastName, user.Username)
+	return err
+}
+
+func toGRPCUser(user *model.User) *services.User {
+	return &services.User{
+		Id:          user.ID.String(),
+		FirstName:   user.FirstName,
+		LastName:    user.LastName,
+		PhoneNumber: user.PhoneNumber,
+		Username:    user.Username,
 	}
 }
